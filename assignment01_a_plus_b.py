3eb9b276385865d274f5e1ffeed93a5747fd4d86
Suhail Ahamed <suhail511@gmail.com>
2017-11-30 11:43:39 +0400

    change back to original code, add #python3 comment

--- assignment01_a_plus_b.py
+++ assignment01_a_plus_b.py
@@ -1,16 +1,17 @@
+#python3
 '''
     Read two numbers separated by a space and return thier sum
 '''
 
-# a = input()
-# a = a.split(' ')
+a = input()
+a = a.split(' ')
+
+print (int(a[0])+int(a[1]))
+
+# import sys
 #
-# print (int(a[0])+int(a[1]))
-
-import sys
-
-input = sys.stdin.read()
-tokens = input.split()
-a = int(tokens[0])
-b = int(tokens[1])
-print(a + b)
+# input = sys.stdin.read()
+# tokens = input.split()
+# a = int(tokens[0])
+# b = int(tokens[1])
+# print(a + b)
