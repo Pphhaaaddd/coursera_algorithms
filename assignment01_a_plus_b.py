bd4e7956e97b7859376f38972df3a93e279ea812
Suhail Ahamed <suhail511@gmail.com>
2017-11-30 11:39:15 +0400

    change assignment01 to starter code

--- assignment01_a_plus_b.py
+++ assignment01_a_plus_b.py
@@ -2,7 +2,15 @@
     Read two numbers separated by a space and return thier sum
 '''
 
-a = input()
-a = a.split(' ')
+# a = input()
+# a = a.split(' ')
+#
+# print (int(a[0])+int(a[1]))
 
-print (int(a[0])+int(a[1]))
+import sys
+
+input = sys.stdin.read()
+tokens = input.split()
+a = int(tokens[0])
+b = int(tokens[1])
+print(a + b)
